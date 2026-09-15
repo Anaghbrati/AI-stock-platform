207e1cbb780dca36a2bf2b238b06bdeba0989709
Anaghbrati Sinha Ray <anaghbratisinharay2006@gmail.com>
2026-09-15 11:35:25 +0530

    Migrate middleware to Next.js 16 proxy

--- market-data-service/app/main.py
+++ market-data-service/app/main.py
@@ -1,3 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -38,10 +44,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
+    # allow_origins=[
+    #     "http://localhost:3000",
+    #     "http://127.0.0.1:3000",
+    # ],
+
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    os.getenv("NEXT_PUBLIC_APP_URL", "http://localhost:3000"),
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
